2ddf4bc12bb14ae0079490c0397fd2f2362d5525
Mohamed Ould El Kory <ouldelkory@gmail.com>
2021-02-08 14:57:54 +0000

    first commit

---
+++ Section 1/basic_types.js
@@ -0,0 +1,10 @@
+var a = 10;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color[2];
+// Displays 'Green ok'
+console.log(colorName);
---
+++ Section 1/basic_types.ts
@@ -0,0 +1,12 @@
+
+let my_a:number = 10;
+
+enum Color {
+    Red = 1,
+    Green,
+    Blue,
+  }
+let colorName: string = Color[2];
+  
+// Displays 'Green ok2'
+console.log(colorName);
